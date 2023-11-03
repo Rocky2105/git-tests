5bd26904a939595c6420b0b8a23bd32d66c63a6b
Rodrigo Romero <a20300699@ceti.mx>
2023-11-03 12:03:08 -0600

    Changes to program.cs

--- Program.cs
+++ Program.cs
@@ -4,21 +4,4 @@ using System.Net.Mail;
 string fromEmail = "rocobros21@gmail.com";
 string fromPassword = "zzkp tljs ofdd jkjh";
 
-MailMessage message = new MailMessage()
-{
-    From = new MailAddress(fromEmail),
-    Subject = "Test subject",
-    Body = "Test body",
-};
-message.To.Add(new MailAddress("a20300699@ceti.mx"));
-
-var smtpClient = new SmtpClient("smtp.gmail.com")
-{
-    Port=587,
-    Credentials= new NetworkCredential(fromEmail, fromPassword),
-    EnableSsl=true,
-};
-
 System.Console.WriteLine("Email enviado");
-
-smtpClient.Send(message);
